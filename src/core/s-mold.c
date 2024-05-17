orx/Rebol3

commit 7bb6e7b5507ade30c1c17b5f2d1ed0d63f21c437

FIX: special cases when `mold/all` paths, emails, tags and urls resolves: https://github.com/Oldes/Rebol-issues/issues/2022 resolves: https://github.com/Oldes/Rebol-issues/issues/863

src/core/s-mold.c
@@ -744,24 +744,24 @@ STOID Mold_Block(REBVAL *value, REB_MOLD *mold, REBFLG molded)
 	REBYTE *sep = NULL;
 	REBOOL all = GET_MOPT(mold, MOPT_MOLD_ALL);
 	REBSER *series = mold->series;
-	REBFLG over = FALSE;
-
 
 	if (SERIES_WIDE(VAL_SERIES(value)) == 0)
 		Crash(RP_BAD_WIDTH, sizeof(REBVAL), 0, VAL_TYPE(value));
 
 	// Optimize when no index needed:
-	if (VAL_INDEX(value) == 0 && !IS_MAP(value)) // && (VAL_TYPE(value) <= REB_LIT_PATH))
+	if (VAL_INDEX(value) == 0)
 		all = FALSE;
+	// Force construction syntax in special path cases:
+	if (ANY_PATH(value) && (VAL_TAIL(value) <= 1 || !ANY_WORD(VAL_BLK(value))))
+		all = TRUE;
 
-	// If out of range, do not cause error to avoid error looping.
-	if (VAL_INDEX(value) >= VAL_TAIL(value)) over = TRUE; // Force it into []
+	// Reset index if it is over series tail: (a: [1 2]  b: tail a  clear a  mold b)
+	if (VAL_INDEX(value) > VAL_TAIL(value))
+		VAL_INDEX(value) = VAL_TAIL(value);
 
-	if (all || (over && !IS_BLOCK(value) && !IS_PAREN(value))) {
+	if (all) {
 		SET_FLAG(mold->opts, MOPT_MOLD_ALL);
 		Pre_Mold(value, mold); // #(block! part
-		//if (over) Append_Bytes(mold->series, "[]");
-		//else
 		Mold_Block_Series(mold, VAL_SERIES(value), 0, 0);
 		Post_Mold(value, mold);
 	}
@@ -799,8 +799,10 @@ STOID Mold_Block(REBVAL *value, REB_MOLD *mold, REBFLG molded)
 			break;
 		}
 
-		if (over) Append_Bytes(mold->series, sep ? cs_cast(sep) : "[]");
-		else Mold_Block_Series(mold, VAL_SERIES(value), VAL_INDEX(value), sep);
+		if (VAL_TAIL(value)==0)
+			Append_Bytes(mold->series, sep ? cs_cast(sep) : "[]");
+		else 
+			Mold_Block_Series(mold, VAL_SERIES(value), VAL_INDEX(value), sep);
 
 		if (VAL_TYPE(value) == REB_SET_PATH)
 			if (molded) Append_Byte(series, ':');
@@ -1149,7 +1151,7 @@ STOID Mold_Error(REBVAL *value, REB_MOLD *mold, REBFLG molded)
 	ASSERT2(ser, RP_NO_BUFFER);
 
 	// Special handling of string series: {
-	if (ANY_STR(value) && !IS_TAG(value)) {
+	if (ANY_STR(value)) {
 
 		// Forming a string:
 		if (!molded) {
@@ -1158,9 +1160,10 @@ STOID Mold_Error(REBVAL *value, REB_MOLD *mold, REBFLG molded)
 		}
 
 		// Special format for ALL string series when not at head:
-		if (GET_MOPT(mold, MOPT_MOLD_ALL) && VAL_INDEX(value) != 0) {
-			Mold_All_String(value, mold);
-			return;
+		if (GET_MOPT(mold, MOPT_MOLD_ALL)
+			&& (VAL_INDEX(value) != 0 || VAL_TYPE(value) >= REB_EMAIL && VAL_TAIL(value) == 0)) {
+				Mold_All_String(value, mold);
+				return;
 		}
 	}
 
@@ -1244,18 +1247,17 @@ STOID Mold_Error(REBVAL *value, REB_MOLD *mold, REBFLG molded)
 
 	case REB_EMAIL:
 	case REB_URL:
-		if (VAL_LEN(value) == 0) {
-			Append_Bytes(ser, VAL_TYPE(value) == REB_EMAIL ? "#(email! \"\")" : "#(url! \"\")");
-			break;
+		if (GET_MOPT(mold, MOPT_MOLD_ALL)
+			&& NOT_FOUND == Find_Str_Char(VAL_SERIES(value), 0, 0,
+				VAL_TAIL(value), 1, VAL_TYPE(value) == REB_EMAIL ? '@' : ':', 0))
+		{
+			Mold_All_String(value, mold);
+			return;
 		}
 		Mold_Url(value, mold);
 		break;
 
 	case REB_TAG:
-		if (GET_MOPT(mold, MOPT_MOLD_ALL) && VAL_INDEX(value) != 0) {
-			Mold_All_String(value, mold);
-			return;
-		}
 		Mold_Tag(value, mold);
 		break;
 

src/core/l-scan.c
@@ -1859,7 +1859,8 @@ extern REBSER *Scan_Full_Block(SCAN_STATE *scan_state, REBYTE mode_char);
 			if (!IS_ERROR(value)){
 				emitbuf->tail++; // Protect the block from GC
 				if (!Construct_Value(value, block)) {
-					if (IS_END(value)) Set_Block(value, block);
+					if (IS_END(value))
+						Set_Block(value, block);
 					if (GET_FLAG(scan_state->opts, SCAN_RELAX)) {
 						block = Make_Error(RE_MALCONSTRUCT, value, 0, 0);
 						SET_ERROR(value, RE_PAST_END, block);

src/core/t-block.c
@@ -66,11 +66,14 @@ static void No_Nones_Or_Logic(REBVAL *arg) {
 	REBCNT i;
 
 	if (!ANY_BLOCK(data)) return FALSE;
-	if (type >= REB_PATH && type <= REB_LIT_PATH)
-		if (!ANY_WORD(VAL_BLK(data))) return FALSE;
-
-	*out = *data++;
+	// set the output first...
+	*out = *data;
 	VAL_SET(out, type);
+	// ... to allow construction of empty path types too
+	if (IS_END(VAL_BLK(data)) && type >= REB_PATH && type <= REB_LIT_PATH)
+		return TRUE;
+	// and update the index, if needed...
+	data++;
 	i = IS_INTEGER(data) ? Int32(data) - 1 : 0;
 	if (i > VAL_TAIL(out)) i = VAL_TAIL(out); // clip it
 	VAL_INDEX(out) = i;

src/include/sys-core.h
@@ -206,6 +206,7 @@ enum REB_Mold_Opts {
 };
 
 #define GET_MOPT(v, f) GET_FLAG(v->opts, f)
+#define SET_MOPT(v, f) SET_FLAG(v->opts, f)
 
 // Special flags for decimal formatting:
 #define DEC_MOLD_PERCENT 1  // follow num with %

src/tests/units/lexer-test.r3
@@ -167,9 +167,12 @@ Rebol [
 	--test-- "Invalid char"
 		--assert all [error? e: try [load {2#"a"}] e/id = 'invalid]
 
-	--test-- "Invalid path construction"
+	--test-- "Path construction not using words"
 	;@@ https://github.com/Oldes/Rebol-issues/issues/863
-		--assert all [error? e: try [load {#(path! [0])}] e/id = 'malconstruct]
+		--assert all [
+			path? p: try [transcode/one {#(path! [0])}]
+			integer? first p
+		]
 
 	--test-- "Invalid file"
 	;@@ https://github.com/Oldes/Rebol-issues/issues/1415

src/tests/units/mold-test.r3
@@ -286,6 +286,47 @@ Rebol [
 		--assert "#(path! [p p] 3)" = mold/all next next 'p/p
 		--assert "#(block! [a b] 3)" = mold/all next next [a b]
 
+	--test-- "mold/all series"
+	;@@ https://github.com/Oldes/Rebol-issues/issues/2022
+		--assert {#(email! "")}      == mold/all #(email! "")
+		--assert {#(email! "a")}     == mold/all #(email! "a")
+		--assert {#(url! "")}        == mold/all #(url! "")
+		--assert {#(url! "a")}       == mold/all #(url! "a")
+		--assert {#(tag! "")}        == mold/all #(tag! "")
+		--assert {#(image! 0x0 #{})} == mold/all #(image! 0x0 #{})
+		--assert {#(path! [])}       == mold/all quote #(path! [])
+		--assert {#(set-path! [])}   == mold/all quote #(set-path! [])
+		--assert {#(get-path! [])}   == mold/all quote #(get-path! [])
+		--assert {#(lit-path! [])}   == mold/all quote #(lit-path! [])
+		--assert {#(path! [a])}      == mold/all quote #(path! [a])
+		--assert {#(set-path! [a])}  == mold/all quote #(set-path! [a])
+		--assert {#(get-path! [a])}  == mold/all quote #(get-path! [a])
+		--assert {#(lit-path! [a])}  == mold/all quote #(lit-path! [a])
+		--assert {#(path! [])}       == mold/all clear quote  a/b
+		--assert {#(set-path! [])}   == mold/all clear quote  a/b:
+		--assert {#(get-path! [])}   == mold/all clear quote :a/b
+		--assert {#(lit-path! [])}   == mold/all clear quote 'a/b
+		--assert {#(path! [b])}      == mold/all remove quote  a/b
+		--assert {#(set-path! [b])}  == mold/all remove quote  a/b:
+		--assert {#(get-path! [b])}  == mold/all remove quote :a/b
+		--assert {#(lit-path! [b])}  == mold/all remove quote 'a/b
+
+	--test-- "mold/all block with index over tail"
+		a: [1 2 3 4]
+		b: tail a
+		clear a 
+		--assert "#(block! [])" == mold/all b
+		a: 'a/b/c/d/e
+		b: tail a
+		clear a 
+		--assert "#(path! [])" == mold/all b
+	--test-- "mold/all string with index over tail"
+		a: "12345678"
+		b: tail a
+		clear a 
+		--assert {#(string! "" 9)} == mold/all b
+
+
 ===end-group=== 
 
 

commit 59235186abfb344728fc6b83327c6084ad2ba6d6

FIX: regression from https://github.com/Oldes/Rebol3/commit/7bb6e7b5507ade30c1c17b5f2d1ed0d63f21c437 (appending to `tag!` has different rules)

src/core/s-mold.c
@@ -1151,7 +1151,7 @@ STOID Mold_Error(REBVAL *value, REB_MOLD *mold, REBFLG molded)
 	ASSERT2(ser, RP_NO_BUFFER);
 
 	// Special handling of string series: {
-	if (ANY_STR(value)) {
+	if (ANY_STR(value) && !IS_TAG(value)) { // tag! has different rules!
 
 		// Forming a string:
 		if (!molded) {