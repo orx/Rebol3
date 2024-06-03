f1cab27e195b3957baf4bd59c9614d009ac1d524
Oldes Huhuman <oldes.huhuman@gmail.com>
2024-06-03 15:33:41 +0200

    CHANGE: update Brotli sources to version 1.1.0

--- src/core/brotli/enc/hash.h
+++ src/core/brotli/enc/hash.h
@@ -10,8 +10,6 @@
 #ifndef BROTLI_ENC_HASH_H_
 #define BROTLI_ENC_HASH_H_
 
-#pragma warning(disable : 4334)
-
 #include <stdlib.h>  /* exit */
 #include <string.h>  /* memcmp, memset */
 
@@ -80,8 +78,7 @@ typedef struct HasherSearchResult {
      for this use.
    * The number has been tuned heuristically against compression benchmarks. */
 static const uint32_t kHashMul32 = 0x1E35A7BD;
-static const uint64_t kHashMul64 = BROTLI_MAKE_UINT64_T(0x1E35A7BD, 0x1E35A7BD);
-static const uint64_t kHashMul64Long =
+static const uint64_t kHashMul64 =
     BROTLI_MAKE_UINT64_T(0x1FE35A7Bu, 0xD3579BD3u);
 
 static BROTLI_INLINE uint32_t Hash14(const uint8_t* data) {
@@ -724,11 +721,8 @@ static BROTLI_INLINE size_t LookupAllCompoundDictionaryMatches(
   return total_found;
 }
 
-
 #if defined(__cplusplus) || defined(c_plusplus)
 }  /* extern "C" */
 #endif
-
-#pragma warning(default : 4334)
 
 #endif  /* BROTLI_ENC_HASH_H_ */
